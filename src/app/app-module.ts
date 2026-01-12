0daf13f1cc712efa92bc6c7d2dd19fc55f6142ce
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-01-12 09:30:54 -0700

    Component Basics
    The Basics

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { App } from './app';
-import { ServerComponent } from './server/server.component';
 import { Servers } from './servers/servers';
+import { WarningAlert } from './warning-alert/warning-alert';
+import { SuccessAlert } from './success-alert/success-alert';
 
 @NgModule({
   declarations: [
     App,
-    ServerComponent,
-    Servers
+    Servers,
+    WarningAlert,
+    SuccessAlert
   ],
   imports: [
     BrowserModule,
---
+++ src/app/warning-alert/warning-alert.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  standalone: false,
+  templateUrl: './warning-alert.html',
+  styleUrl: './warning-alert.css',
+})
+export class WarningAlert {
+
+}
